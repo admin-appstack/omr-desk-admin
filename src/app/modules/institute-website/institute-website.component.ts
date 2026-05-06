763a9797afb91f2b99fa1ce41ba086302014a26f
Mohammad Salik <msalik42@gmail.com>
2026-05-07 00:57:55 +0530

    Added a toggle to make website live/offline

--- src/app/modules/institute-website/institute-website.component.ts
+++ src/app/modules/institute-website/institute-website.component.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { TemplateGridComponent } from './components/template-grid/template-grid.component';
 import { WebsiteSettingsComponent } from './components/website-settings/website-settings.component';
 import { PageManagementComponent } from './components/page-management/page-management.component';
@@ -17,6 +18,7 @@ import { PageManagementComponent } from './components/page-management/page-manag
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
+    MatSnackBarModule,
     TemplateGridComponent,
     WebsiteSettingsComponent,
     PageManagementComponent
@@ -27,10 +29,24 @@ import { PageManagementComponent } from './components/page-management/page-manag
 export class InstituteWebsiteComponent {
   selectedTemplate = signal(1);
   activeStep = 0;
+  isLive = true;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
   onTemplateSelected(id: number) {
     this.selectedTemplate.set(id);
+  }
+
+  toggleLive() {
+    this.isLive = !this.isLive;
+    const message = this.isLive
+      ? '🟢 Website is now LIVE and publicly accessible.'
+      : '🔴 Website has been taken OFFLINE. Visitors will see a maintenance page.';
+    this.snackBar.open(message, 'Dismiss', {
+      duration: 4000,
+      panelClass: this.isLive ? 'snack-live' : 'snack-offline',
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
   }
 }
